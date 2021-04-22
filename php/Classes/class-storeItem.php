16f1e75b679402790b435567023a2a5e202e97eb
CodeCanna <codecannamw@gmail.com>
2021-04-21 22:51:45 -0600

    Added a getItemById function to the store class.

--- php/Classes/class-storeItem.php
+++ php/Classes/class-storeItem.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace mos_curios\StoreItem;
 
+use Exception;
 
 /**
  * @param uuid $itemId
@@ -10,7 +12,8 @@ namespace mos_curios\StoreItem;
  * @param string $itemImage
  * @param bool $onSale
  */
-class StoreItem {
+class StoreItem
+{
     public $itemId;
     public $itemName;
     public $itemDescription;
@@ -18,10 +21,26 @@ class StoreItem {
     public $itemImage;
     public $onSale;
 
+
+    public static function getItemById($db, string $id)
+    {
+        // Check db connection
+        if (pg_connection_status($db) !== PGSQL_CONNECTION_OK) {
+            throw new Exception("There was a problem connecting to the database...");
+        }
+        $query = "SELECT * FROM store_items WHERE store_items.item_id = '$id'";
+        pg_send_query($db, $query);
+
+        $result = pg_get_result($db);
+
+        var_dump(pg_fetch_row($result));
+    }
+
     /**
      * 
      */
-    function __construct($itemId, string $itemName, string $itemDescription, float $itemPrice, $itemImage, ?bool $onSale) {
+    public function __construct($itemId, string $itemName, string $itemDescription, float $itemPrice, $itemImage, ?bool $onSale)
+    {
         /*
         $this->setItemId($itemId);
         $this->setItemName($itemName);
--- php/api/mos_store.php
+++ php/api/mos_store.php
@@ -5,6 +5,9 @@ require_once(dirname(__DIR__, 1) . '/Classes/class-storeItem.php');
 use Ramsey\Uuid\Uuid;
 use mos_curios\StoreItem\StoreItem;
 
+
+header("Content-type: application/json");
+
 // Create empty reply
 $reply = new stdClass();
 $reply->status = 200;
@@ -21,8 +24,9 @@ if (isset($_SERVER["REQUEST_METHOD"])) {
 
     switch ($httpMethod) {
         case "GET":
-            // Handl GET method
-            echo (json_encode('{data: GET}'));
+            // Handle GET method
+            StoreItem::getItemById($pgConnection, "e3ad9857-430c-4873-9ef5-2d1dc1e20148");
+
             break;
         case "POST":
             // Handle POST method
@@ -31,9 +35,10 @@ if (isset($_SERVER["REQUEST_METHOD"])) {
             $folder = "img/" . $filename;
             $item = new StoreItem(Uuid::uuid4()->toString(), $_POST["itemName"], $_POST["itemDescription"], $_POST["itemPrice"], $filename, false);
 
-            addItem($pgConnection, $item);
+            addItem($pgConnection, $item, $reply);
 
             move_uploaded_file($tempname, $folder);
+
             break;
         case "PUT":
             // Handle PUT method
@@ -50,11 +55,24 @@ if (isset($_SERVER["REQUEST_METHOD"])) {
     }
 }
 
-function addItem($db, StoreItem $item)
+function addItem($db, StoreItem $item, $reply)
 {
+    // Check connection status
+    if (pg_connection_busy($db)) {
+        throw new Exception("Connection Busy!");
+    }
     // DON'T FORGET THE SINGLE QUOTES!!!!!
     // Forgetting the single quotes will result in a SQL syntax error
     $query = "INSERT INTO store_items VALUES ('$item->itemId', '$item->itemName', '$item->itemDescription', '$item->itemPrice', '$item->onSale');";
 
-    $dataThing = pg_query($db, $query);
+    // Check if query ran successfully
+    $dbq = pg_query($db, $query);
+    if (! $dbq) {
+        throw new Exception("Unable to execute query");
+    }
+
+    $reply->status = 200;
+    $reply->data = "Store Item Added!";
 }
+
+echo (json_encode($reply));
