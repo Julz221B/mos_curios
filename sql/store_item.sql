6f2303d257e2208e4fa59fa3b0658a95c06d4597
CodeCanna <codecannamw@gmail.com>
2021-04-25 23:28:58 -0600

    Made item_id the primary key form store_items table and added item_date_added field.

--- sql/store_item.sql
+++ sql/store_item.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS store_items;
 
 CREATE TABLE store_items (
-    item_id uuid UNIQUE NOT NULL,
+    item_id uuid UNIQUE PRIMARY KEY NOT NULL,
     item_name text NOT NULL,
     item_description text NOT NULL,
     item_price float NOT NULL,
     item_image text NOT NULL, 
-    item_date_added datetime NOT NULL
+    item_date_added date NOT NULL DEFAULT(NOW())
 );
